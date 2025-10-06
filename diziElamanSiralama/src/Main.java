604c4ece7a6206555b35cd2257d7bc531bec89af
Ahmet Tarık <tarik.efeoglu03@gmail.com>
2025-10-06 16:48:33 +0300

    Add files via upload

---
+++ diziElamanSiralama/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dizinin boyutu n : ");
+        int n = input.nextInt();
+        int[] numbers = new int[n];
+
+        System.out.println("Dizinin elemanlarını giriniz :");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + ". Elemanı : ");
+            numbers[i] = input.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.print("Sıralama : ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
